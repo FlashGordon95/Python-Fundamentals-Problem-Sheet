47934d450dc0848898226d613b8cc5e534c68c2e
Ryan Gordon <ryangordon210@gmail.com>
2017-09-21 17:27:03 +0100

    Problems 8 and 10

---
+++ 08-MergeListSort.py
@@ -0,0 +1,23 @@
+
+'''
+Merge List and Sort
+Author: Ryan Gordon
+Date: 21/09/17
+Write a function that merges two sorted lists into a new sorted list. [1,4,6],[2,3,5] → [1,2,3,4,5,6].
+
+'''
+
+# Specify two lists
+listone = [99,1 ,4 ,6,8,24]
+listtwo = [2 ,3 ,5]
+
+# Create third list from the first 2
+listthree = listone + listtwo
+print("Before sort :" )
+print(listthree)
+listthree.sort()
+
+
+
+print ("After sort : ")
+print (listthree)
---
+++ 10-ReverseString.py
@@ -0,0 +1,18 @@
+'''
+Reverse String
+Author: Ryan Gordon
+Date: 21/09/17
+Write a function to reverse a string.
+
+'''
+
+
+def reverse(text):
+    if len(text) <= 1:
+        return text
+    # Take the first char and paste it onto the end of the string
+    return reverse(text[1:]) + text[0]
+# Test String
+string = "Yo Yo Yo , its me a Mario"
+print(string)
+print(reverse(string))
