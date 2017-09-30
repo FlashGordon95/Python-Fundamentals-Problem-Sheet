1c9bce3eb31e6b24b88884198180d4af5b29f5d6
Ryan Gordon <ryangordon210@gmail.com>
2017-09-30 14:14:39 +0100

    Problems 5+6 Expected output + Enchancement for 6

--- 05-GuessingGame.py
+++ 05-GuessingGame.py
@@ -17,9 +17,9 @@ import random
 
 guessedCorrect = False
 tries = 0
-previousGuesses = set()
-targetNumber = random.randint(0, 20)
-
+previousGuesses = set() #Set only allows unique values 
+targetNumber = random.randint(1, 20) # Rand between 1 and 20
+# guessedCorrect flag starts at false and is switched to true when guess is correct
 while (guessedCorrect is False):
     try:
         var = int(input("Previous tries "+str(tries)+"\n Please enter a number to guess: "))
@@ -27,8 +27,8 @@ while (guessedCorrect is False):
 
         # Check if they guessed correctly
         if (targetNumber == var):
-        print("You guessed correct! Number is "+ str(targetNumber))
-        guessedCorrect=True
+            print("You guessed correct! Number is "+ str(targetNumber))
+            guessedCorrect=True
 
         else:
             #If they enter the same incorrect guess dont add to attempts
@@ -38,12 +38,35 @@ while (guessedCorrect is False):
         
             #number is too small
             if(var<targetNumber):
-                print("Guess is too small")
+                print("Guess is too small, go higher!")
             #number is too large    
             elif(var>targetNumber):
-                print("Guess is too large")
+                print("Guess is too large, go lower!")
         #Catch when a non number or empty input is entered        
-        except ValueError:
-            print('Non-numeric data found in the file. Try again')
+    except ValueError:
+         print('Non-numeric data found in the file. Try again')
     
             
+'''
+Output 
+Previous tries 0
+ Please enter a number to guess: 6
+you entered 6
+Guess is too small, go higher!
+Previous tries 1
+ Please enter a number to guess: 9
+you entered 9
+Guess is too small, go higher!
+Previous tries 2
+ Please enter a number to guess: 13
+you entered 13
+Guess is too small, go higher!
+Previous tries 3
+ Please enter a number to guess: 16
+you entered 16
+Guess is too small, go higher!
+Previous tries 4
+ Please enter a number to guess: 19
+you entered 19
+You guessed correct! Number is 19
+'''
--- 06-LargestSmallest.py
+++ 06-LargestSmallest.py
@@ -4,15 +4,28 @@ Author: Ryan Gordon
 Date: 21/09/17
 
 Write a function that returns the largest and smallest elements in a list.
+
+Enhancement: Generate a random list of integers and return the largest and smallest elements 
 Ryan Gordon -
 
 """
-
+import random
 
 testList = [1,2,3,4,5]
+testList2 = [int(10*random.random()) for i in range(10)]
 def largeSmall(list):
 
     return min(list), max(list)
 
 
 print(largeSmall(testList))
+print(testList2)
+print(largeSmall(testList2))
+
+"""
+Output (example):
+
+(1, 5)
+[5, 7, 6, 7, 2, 2, 2, 4, 3, 4]
+(2, 7)
+"""
